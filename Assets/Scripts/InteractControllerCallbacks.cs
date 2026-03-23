4b341c95e3294f5b94f01c69c37adf7690d5ff6f
Noah <NoahKRAllen@gmail.com>
2026-03-22 21:42:41 -0700

    Relearning new input system

--- Assets/Scripts/InteractControllerCallbacks.cs
+++ Assets/Scripts/InteractControllerCallbacks.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class InteractController : MonoBehaviour
+public class InteractControllerCallbacks : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
